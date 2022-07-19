f0ae4c016fa267039e8405e6b819998b153d6795
aruslantsev <andreiruslantsev@gmail.com>
2022-07-19 17:35:04 +0300

    update

--- gentoo/stage4/mkstage4.sh
+++ gentoo/stage4/mkstage4.sh
@@ -1,7 +1,7 @@
 mount /dev/mapper/hd-root /mnt/gentoo
 mount /dev/sda1 /mnt/gentoo/boot/efi
 cd /mnt/gentoo
-tar cjpvf ~/stage4.tar.bz2 . -X ~/stage4.excl
+tar cjpvf ~/stage4.tar.bz2 -X ~/stage4.excl .
 cd
 umount /dev/sda1
 umount /dev/mapper/hd-root
